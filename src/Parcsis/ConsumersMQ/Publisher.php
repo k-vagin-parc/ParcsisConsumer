9831cb6c9ab738870c00e43790a6d2abbaf305fa
Kirill Vagin <k.vagin@parcsis.org>
2015-02-02 17:13:56 +0400

    возвращаемся к PECL AMQP

--- src/Parcsis/ConsumersMQ/Publisher.php
+++ src/Parcsis/ConsumersMQ/Publisher.php
@@ -11,14 +11,6 @@ class Publisher
 {
 	public static function publish(\MessageBaseUntyped $msgBody, $exchangeName)
 	{
-		/** @var \PhpAmqpLib\Connection\AMQPConnection $connect */
-		$connect = \ConnectMQ::getConnect();
 
-		$message = new \PhpAmqpLib\Message\AMQPMessage(serialize($msgBody), ['content_type' => 'text/plain', 'delivery_mode' => 2]);
-
-		$channel = $connect->channel();
-		$channel->queue_bind($msgBody->message_type, $exchangeName, $msgBody->getKey());
-		$channel->basic_publish($message, $exchangeName, $msgBody->getKey());
-		$channel->close();
 	}
 }
