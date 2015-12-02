1a2f7359b9c7353784fd8100190eb7dd5ea15522
kir <k.vagin@parcsis.org>
2015-12-02 15:07:20 +0400

    портировал костыльный метод для определения кол-ва сообщений в очереди

---
+++ src/Parcsis/ConsumersMQ/Helper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @author k.vagin
+ */
+
+namespace Parcsis\ConsumersMQ;
+
+
+class Helper
+{
+    /**
+     * вернуть количество сообщение в очереди на данный момент по ее имени
+     * @param string $queueName
+     * @param string $user
+     * @param string $password
+     * @param string $host
+     * @param int $httpPort  порт web интерфейса кролика
+     * @return int
+     * @throws \Exception
+     */
+    public static function getQueueMessagesCount($queueName, $user, $password, $host, $httpPort)
+    {
+        $httpPort = sprintf("http://%s:%s@%s:%s/api/queues/%s/%s", $user, $password, $host, $httpPort, '%2F', $queueName);
+
+        $json = file_get_contents($httpPort);
+        $raw = json_decode($json, true);
+
+        if (!array_key_exists('messages', $raw)) {
+            throw new \Exception("bad response!");
+        }
+
+        return (int)$raw['messages'];
+    }
+}
